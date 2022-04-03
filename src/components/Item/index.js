a62c6a8a46623b51a2190d02eff9f58c9f650725
manhtung001 <khongtung001@gmail.com>
2022-04-03 17:01:18 +0700

    group item

--- src/components/Item/index.js
+++ src/components/Item/index.js
@@ -1,29 +1,44 @@
-import React from 'react'
+import { HeartOutlined, ReloadOutlined, SearchOutlined, ShoppingCartOutlined, StarFilled } from '@ant-design/icons'
+import React, { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux'
 import { Link } from 'react-router-dom'
+import { ProductData } from '../../action'
+import productApi from '../../api/productApi'
 import './style.css'
 
 
-const Item = (value, onDetailProduct) => {
-    let money = Number(value.price);
-    money = money.toLocaleString('vi', { style: 'currency', currency: 'VND' })
-    return (
-        // co 1 api tra ve thong tin 1 san pham
-        <Link onClick={() => onDetailProduct(value._id)} to={`/products/${value.slug}`} className="product__item__title">
-        <div className="col-xl-3 col-sm-6 col-md-4 product__items">
-            <div className="product__content">
-                <div className="product__content__img">
-                    <img className="img--before" src={value.productImage[0].img} alt="" />
-                    <img className="img--after" src={value.productImage[1].img} alt="" />
-                </div>
-                <div className="product__img--title">Giảm {value.sale}%</div>
-            </div>
-            <Link className="product__item__title">
-            {value.title}
-            </Link>
-            <p className="product__item__price">{money}</p>
-        </div>
-        </Link>
-    )
+const onDetailProduct = (id, dispatch, product) => {
+    let result = null
+    product.forEach((value) => {
+        if (id === value._id) {
+            result = value
+        }
+    })
+    const action = ProductData(result)
+    dispatch(action)
 }
 
-export default Item;
+const showProduct = (products, dispatch) => {
+    let result = null
+    result = products.map((value, index) => {
+        let money = Number(value.price);
+        money = money.toLocaleString('vi', { style: 'currency', currency: 'VND' })
+
+        return (
+            <Link onClick={() => onDetailProduct(value._id, dispatch, products)} to={`/products/${value.slug}`} key={index} className="col-xl-3 col-sm-6 col-md-4 product__items">
+                <div className="product__content">
+                    <div className="product__content__img">
+                        <img className="img--before" src={value.productImage[0].img} alt="" />
+                        <img className="img--after" src={value.productImage[1].img} alt="" />
+                    </div>
+                    <div className="product__img--title">Giảm {value.sale}%</div>
+                </div>
+                <Link onClick={() => onDetailProduct(value._id, dispatch, products)} to={`/products/${value.slug}`} className="product__item__title">{value.title}</Link>
+                <p className="product__item__price">{money}</p>
+            </Link>
+        )
+    })
+    return result
+}
+
+export default showProduct
