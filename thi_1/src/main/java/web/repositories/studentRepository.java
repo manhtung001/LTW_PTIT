9821fd494e154b8daa33520be4ac75d7a9374db0
manhtung001 <khongtung001@gmail.com>
2022-06-21 16:23:33 +0700

    add

--- thi_1/src/main/java/web/repositories/studentRepository.java
+++
@@ -1,9 +0,0 @@
-package web.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import web.models.student;
-
-public interface studentRepository extends CrudRepository<student, String> {
-
-}
